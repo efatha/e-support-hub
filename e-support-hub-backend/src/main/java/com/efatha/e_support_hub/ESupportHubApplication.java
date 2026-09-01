8bf9ccdb601486e8e77f53bc9dcb1f473e335687
efatha <efathabyamungu4@gmail.com>
2026-09-01 15:07:44 +0300

    Initialized the Java boot spring

---
+++ e-support-hub-backend/src/main/java/com/efatha/e_support_hub/ESupportHubApplication.java
@@ -0,0 +1,13 @@
+package com.efatha.e_support_hub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ESupportHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ESupportHubApplication.class, args);
+	}
+
+}
