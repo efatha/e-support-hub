e7f04fcbc0296fede0c76526181517a48e09badc
efatha <efathabyamungu4@gmail.com>
2026-09-01 17:06:58 +0300

    Requesting a Restful Api on the new path

--- e-support-hub-backend/src/main/java/com/efatha/e_support_hub_backend/Home.java
+++ e-support-hub-backend/src/main/java/com/efatha/e_support_hub_backend/Home.java
@@ -1,13 +1,17 @@
 package com.efatha.e_support_hub_backend;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Map;
+
 @RestController
+@RequestMapping("/api")
 public class Home {
     
     @GetMapping("/home")
-    public String HomeController(){
-      return "Welcome to E-Support Hub Backend!";
+    public Map<String, String> HomeController(){
+      return Map.of("message", "Welcome to E-Support Hub Backend!");
     }
 }
